8cd6f1a7de0c523628398876c4bdef4d72315273
Sunshine0213 <138002841+Sunshine0213@users.noreply.github.com>
2023-08-01 05:01:43 -0700

    my portfolio

---
+++ src/data/contactsData.js
@@ -0,0 +1,10 @@
+export const contactsData = {
+    email: 'bluedreambd013@gmail.com',
+    github: '',
+    skype: 'live:.cid.cdd53840b65969db',
+    stackOverflow: '#',
+    telegram: 'redhatn16',
+    phone: '81 479 63 12',
+    devUsername: "bluedream013",
+    address: 'Auckland, New Zealand'
+}
---
+++ src/data/educationData.js
@@ -0,0 +1,9 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'Stanford University',
+        course: 'Bachelor Degree',
+        startYear: '09/2010',
+        endYear: '09/2013'
+    },
+]
